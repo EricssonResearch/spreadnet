46cdc8e10395138ba405c601b27406f381a77e00
haodong_zhao <haodong.zhao.dev@outlook.com>
2022-09-20 22:37:43 +0200

    Modify the loss function. Loss function outputs (losses, num of correct predictions) now.

--- architecture/loss.py
+++ architecture/loss.py
@@ -18,6 +18,8 @@ def loss_fn(data, trainable_gnn: EncodeProcessDecode):
 
     node_logits, edge_logits = trainable_gnn(nodes_data, edge_index, edges_data)
     node_labels, edge_labels = labels
+    node_labels = node_labels.type(torch.int64)
+    edge_labels = edge_labels.type(torch.int64)
     # print("[in loss] node_logits: ", node_logits.size())
     # print("[in loss] node_labels: ", node_labels.size())
     # print("[in loss] edge_logits", edge_logits.size())
@@ -28,15 +30,19 @@ def loss_fn(data, trainable_gnn: EncodeProcessDecode):
         "edges": torch.nn.functional.cross_entropy(edge_logits, edge_labels, reduction='mean'),
     }
 
-    # track the accuracy
+    # track the num of correct predictions
     predicted_node_labels = torch.argmax(node_logits, dim=-1).type(torch.int64)
     predicted_edge_labels = torch.argmax(edge_logits, dim=-1).type(torch.int64)
 
-    accuracies = {
-        "edges":
-            torch.mean(torch.Tensor(node_labels == predicted_node_labels).type(torch.float32)),
-        "nodes":
-            torch.mean(torch.Tensor(edge_labels == predicted_edge_labels).type(torch.float32))
+    node_comps = torch.Tensor(node_labels == predicted_node_labels).type(torch.int64)
+    edge_comps = torch.Tensor(edge_labels == predicted_edge_labels).type(torch.int64)
+
+    corrects = {
+        "nodes": torch.sum(node_comps),
+        "edges": torch.sum(edge_comps)
     }
 
-    return losses, accuracies
+    assert (data.num_nodes >= torch.sum(node_comps))
+    assert (data.num_edges >= torch.sum(edge_comps))
+
+    return losses, corrects
