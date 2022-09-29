9129e66a38bf5358142c1d57d3a95917d53f2119
Vuth <cchvuth@gmail.com>
2022-09-29 09:31:38 +0200

    remove duplicate print

--- experiments/encode_process_decode/generate_dataset.py
+++ experiments/encode_process_decode/generate_dataset.py
@@ -16,12 +16,11 @@ random_seed = data_configs["random_seed"]
 num_nodes_min_max = (data_configs["num_node_min"], data_configs["num_node_max"])
 dataset_size = data_configs["dataset_size"]
 dataset_path = os.path.join(os.path.dirname(__file__), data_configs["dataset_path"])
-
 raw_path = dataset_path + "/raw"
 
 if not os.path.exists(raw_path):
     os.makedirs(raw_path)
-# raw_path = str(get_project_root()) + dataset_path + "raw/"
+
 # ------------------------------------------
 if __name__ == "__main__":
     graph_generator = GraphGenerator(
@@ -29,13 +28,12 @@ if __name__ == "__main__":
     ).task_graph_generator()
 
     all_graphs = list()
+
     for idx in range(dataset_size):
         all_graphs.append(nx.node_link_data(next(graph_generator)))
 
     with open(raw_path + "/random.json", "w") as outfile:
         json.dump(all_graphs, outfile, cls=NpEncoder)
-
-    print("Graph Generation Done...\n")
 
     print("Graph Generation Done...\nProcessing...")
     process(dataset_path)
