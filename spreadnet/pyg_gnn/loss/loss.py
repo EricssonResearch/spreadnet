ded055b11ccad365a8dd785580f09787b80f236b
haodong_zhao <haodong.zhao.dev@outlook.com>
2022-09-29 17:12:25 +0200

    Improve loss function, try to fix UserWarning(To copy construct from a tensor).

--- spreadnet/pyg_gnn/loss/loss.py
+++ spreadnet/pyg_gnn/loss/loss.py
@@ -37,12 +37,20 @@ def hybrid_loss(node_pred, edge_pred, node_true, edge_true):
     predicted_node_labels = torch.argmax(node_pred, dim=-1).type(torch.int64)
     predicted_edge_labels = torch.argmax(edge_pred, dim=-1).type(torch.int64)
 
-    node_comps = torch.tensor(
-        (node_true == predicted_node_labels).to(torch.float), device=node_pred.device
-    ).type(torch.int64)
-    edge_comps = torch.tensor(
-        (edge_true == predicted_edge_labels).to(torch.float), device=node_pred.device
-    ).type(torch.int64)
+    node_comps = (
+        (node_true == predicted_node_labels)
+        .clone()
+        .detach()
+        .type(torch.int64)
+        .to(node_pred.device)
+    )
+    edge_comps = (
+        (edge_true == predicted_edge_labels)
+        .clone()
+        .detach()
+        .type(torch.float)
+        .to(node_pred.device)
+    )
 
     corrects = {"nodes": torch.sum(node_comps), "edges": torch.sum(edge_comps)}
 
