866876bf0bce6e41ba9a36e8de662dfbd4e01a8e
Haoyuan-L <haoyuan.li.9654@gmail.com>
2022-10-19 12:08:34 +0200

    update SPGAT model

--- spreadnet/pyg_gnn/models/graph_attention_network/sp_gat.py
+++ spreadnet/pyg_gnn/models/graph_attention_network/sp_gat.py
@@ -1,8 +1,6 @@
 """Basic GAT model.
-
 Usage:
     python train.py [--config config_file_path]
-
 @Time    : 10/03/2022 2:05 PM
 @Author  : Haoyuan Li
 """
@@ -17,24 +15,22 @@ from torch_geometric.nn import GATConv, MLP, Linear
 class SPGATNet(torch.nn.Module):
     def __init__(
             self,
-            num_hidden_layers: int,
-            in_channels: Union[int, Tuple[int, int]],
-            hidden_channels: int,
-            out_channels: int,
-            heads: int = 1,
-            dropout: float = 0.5,
-            concat: bool = False,
-            add_self_loops: bool = False,
-            bias: bool = True,
-            return_attention_weights: bool = True,
-            edge_in_channels: int = 5,
-            edge_hidden_channels: int = 16,
-            edge_out_channels: int = 2,
-            edge_num_layers: int = 2,
-            edge_bias: bool = True
+            # in_channels: int,
+            # hidden_channels: int,
+            # out_channels: int,
+            # heads: int = 1,
+            # dropout: float = 0.6,
+            # concat: bool = False,
+            # add_self_loops: bool = True,
+            # bias: bool = True,
+            # return_attention_weights: bool = True,
+            # edge_in_channels: int = 5,
+            # edge_hidden_channels: int = 16,
+            # edge_out_channels: int = 2,
+            # edge_num_layers: int = 2,
+            # edge_bias: bool = True
     ):
         """
-
                 Args:
                     num_hidden_layers:
                     in_channels: Size of each input sample.
@@ -51,88 +47,92 @@ class SPGATNet(torch.nn.Module):
                     edge_out_channels:
                     edge_num_layers:
                     edge_bias:
-
                 """
         super(SPGATNet, self).__init__()
 
-        # if in_channels is not None:
-        #    sizes = [in_channels] + [hidden_channels] * num_hidden_layers + [out_channels]
-        # else:
-        #    raise ValueError
-
-        self.linear = Linear(in_channels, hidden_channels)
-        self.node_encoder = Linear(in_channels, hidden_channels*2)
-        self.edge_encoder = Linear(1, hidden_channels*2)
+        self.node_encoder = Linear(3, 64)
+        self.edge_encoder = Linear(1, 64)
 
         self.conv1 = GATConv(
-            in_channels=in_channels,
-            out_channels=hidden_channels,
-            heads=heads,
-            concat=concat,
-            dropout=dropout,
-            add_self_loops=add_self_loops,
-            bias=bias,
-            return_attention_weights=return_attention_weights
+            in_channels=64,
+            out_channels=64,
+            heads=8,
+            concat=True,
+            dropout=0.6,
+            add_self_loops=True,
+            bias=True,
+            return_attention_weights=True
         )
         self.conv2 = GATConv(
-            in_channels=hidden_channels,
-            out_channels=hidden_channels,
-            heads=heads,
-            concat=concat,
-            dropout=dropout,
-            add_self_loops=add_self_loops,
-            bias=bias,
-            return_attention_weights=return_attention_weights
+            in_channels=64 * 8,
+            out_channels=64,
+            heads=8,
+            concat=True,
+            dropout=0.6,
+            add_self_loops=True,
+            bias=True,
+            return_attention_weights=True
         )
         self.conv3 = GATConv(
-            in_channels=hidden_channels,
-            out_channels=out_channels,
-            heads=heads,
-            concat=concat,
-            dropout=dropout,
-            add_self_loops=add_self_loops,
-            bias=bias,
-            return_attention_weights=return_attention_weights
+            in_channels=64 * 8,
+            out_channels=64,
+            heads=8,
+            concat=False,
+            dropout=0.6,
+            add_self_loops=True,
+            bias=True,
+            return_attention_weights=True
         )
+
+        self.lin = Linear(64, 2)
+        self.linear = Linear(3, 512)
         self.edge_classifier = MLP(
-            in_channels=edge_in_channels,
-            hidden_channels=edge_hidden_channels,
-            out_channels=edge_out_channels,
-            num_layers=edge_num_layers,
-            bias=edge_bias
+            in_channels=136,
+            hidden_channels=256,
+            out_channels=2,
+            num_layers=2,
+            bias=True
         )
 
     def forward(self, x, edge_index, edge_attr, return_attention_weights):
-
-        #edge_len = edge_attr.size()[0]
         out = self.node_encoder(x)
         edge_attr = self.edge_encoder(edge_attr)
 
-        out, (edge_index, alpha) = self.conv1(x=x,
-                                              edge_index=edge_index,
-                                              edge_attr=edge_attr,
-                                              return_attention_weights=return_attention_weights,
-                                              )
-        out = F.dropout(out, p=0.5, training=self.training)
+        out = F.dropout(out, p=0.3, training=self.training)
+        out, (_, alpha) = self.conv1(x=out,
+                                     edge_index=edge_index,
+                                     edge_attr=edge_attr,
+                                     return_attention_weights=True
+                                     )
+        out = F.elu(out)
+        out = F.dropout(out, p=0.3, training=self.training)
         out = out + self.linear(x)
 
-        out, (edge_index, alpha) = self.conv2(x=out, edge_index=edge_index, edge_attr=alpha, return_attention_weights=return_attention_weights)
-        out = F.dropout(out, p=0.5, training=self.training)
+        out, (_, alpha) = self.conv2(x=out,
+                                     edge_index=edge_index,
+                                     edge_attr=edge_attr,
+                                     return_attention_weights=True
+                                     )
+        out = F.elu(out)
+        out = F.dropout(out, p=0.3, training=self.training)
         out = out + self.linear(x)
 
-        out, (edge_index, alpha) = self.conv3(x=out, edge_index=edge_index, edge_attr=alpha,
-                                              return_attention_weights=return_attention_weights,
-                                              )
-        out = F.dropout(out, p=0.5, training=self.training)
+        out, (_, alpha) = self.conv3(x=out,
+                                     edge_index=edge_index,
+                                     edge_attr=edge_attr,
+                                     return_attention_weights=True
+                                     )
+        node_embedding = F.elu(out)
+        x_out = self.lin(node_embedding)
 
+        node_embeds_src, node_embeds_dst = (
+            node_embedding[edge_index[0]],
+            node_embedding[edge_index[1]],
+        )
 
-        #edge_index = edge_index[:, 0:edge_len]
-        #alpha = alpha[0:edge_len, :]
+        edge_len = edge_attr.size()[0]
+        alpha = alpha[0:edge_len, :]
+        edge_embedding = torch.cat([node_embeds_src, alpha, node_embeds_dst], dim=-1)
+        edge_out = self.edge_classifier(edge_embedding)
 
-        out_src, out_dst = out[edge_index[0]], out[edge_index[1]]
-        edge_feat = torch.cat([out_src, alpha, out_dst], dim=-1)
-
-        edge_out = self.edge_classifier(edge_feat)
-        #out = F.sigmoid(out)
-        #edge_out = F.relu(edge_out)
-        return out, edge_out
+        return x_out, edge_out
