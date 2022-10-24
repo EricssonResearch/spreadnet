3386b5815877adcc6875e50b99474eba6bc548e1
Haoyuan-L <haoyuan.li.9654@gmail.com>
2022-10-24 16:22:48 +0200

    generalize the SPGAT model and wrap the GAT layers

--- spreadnet/pyg_gnn/models/graph_attention_network/sp_gat.py
+++ spreadnet/pyg_gnn/models/graph_attention_network/sp_gat.py
@@ -5,7 +5,6 @@ Usage:
 @Author  : Haoyuan Li
 """
 
-from typing import Union, Tuple
 import torch
 from torch import nn
 import torch.nn.functional as F
@@ -14,117 +13,122 @@ from torch_geometric.nn import GATConv, MLP, Linear
 
 class SPGATNet(torch.nn.Module):
     def __init__(
-            self,
-            # in_channels: int,
-            # hidden_channels: int,
-            # out_channels: int,
-            # heads: int = 1,
-            # dropout: float = 0.6,
-            # concat: bool = False,
-            # add_self_loops: bool = True,
-            # bias: bool = True,
-            # return_attention_weights: bool = True,
-            # edge_in_channels: int = 5,
-            # edge_hidden_channels: int = 16,
-            # edge_out_channels: int = 2,
-            # edge_num_layers: int = 2,
-            # edge_bias: bool = True
+        self,
+        num_hidden_layers: int,
+        in_channels: int,
+        hidden_channels: int,
+        out_channels: int,
+        heads: int,
+        # dropout: float = 0.5,
+        add_self_loops: bool,
+        bias: bool,
+        edge_hidden_channels: int,
+        edge_out_channels: int,
+        edge_num_layers: int,
+        edge_bias: bool,
+        encode_node_in: int,
+        encode_edge_in: int,
+        encode_node_out: int,
+        encode_edge_out: int,
+        concat_hidden_layer: bool = True,
+        concat_out_layer: bool = False,
     ):
         """
-                Args:
-                    num_hidden_layers:
-                    in_channels: Size of each input sample.
-                    hidden_channels: Size of the first hidden layer output sample.
-                    out_channels: Size of each output sample.
-                    heads: Number of multi-head-attentions.
-                    dropout: Dropout probability of the normalized attention coefficients
-                    concat: If 'Flase', the multi-head attentions are averaged instead of concatenated.
-                    add_self_loops: If 'True', add self loops to input graph
-                    bias: If 'True', the layer will learn an additive bias
-                    return_attention_weights:
-                    edge_in_channels:
-                    edge_hidden_channels:
-                    edge_out_channels:
-                    edge_num_layers:
-                    edge_bias:
-                """
+        Args:
+            num_hidden_layers: The number of hidden layers.
+            in_channels: Size of each input sample.
+            hidden_channels: Size of the hidden layer output sample.
+            out_channels: Size of each output sample.
+            heads: Number of multi-head-attentions.
+            #dropout: Dropout probability of the normalized attention coefficients.
+            add_self_loops: If 'True', add self loops to input graph.
+            bias: If 'True', the layer will learn an additive bias.
+            encode_node_in: The input size of node encoder.
+            encode_edge_in: The input size of edge encoder.
+            encode_node_out: The output size of node encoder.
+            encode_edge_out: The output size of edge encoder.
+            concat_hidden_layer: The input and hidden state of the multi-head attentions
+                                 should be concatenated.
+            concat_out_layer: The output of the multi-head attentions should be averaged
+        """
         super(SPGATNet, self).__init__()
+        self.conv = nn.Sequential()
 
-        self.node_encoder = Linear(3, 64)
-        self.edge_encoder = Linear(1, 64)
+        if in_channels is not None:
+            input_sizes = (
+                [in_channels]
+                + [hidden_channels] * num_hidden_layers
+                + [hidden_channels]
+            )
+        else:
+            raise ValueError
 
-        self.conv1 = GATConv(
-            in_channels=64,
-            out_channels=64,
-            heads=8,
-            concat=True,
-            dropout=0.6,
-            add_self_loops=True,
-            bias=True,
-            return_attention_weights=True
-        )
-        self.conv2 = GATConv(
-            in_channels=64 * 8,
-            out_channels=64,
-            heads=8,
-            concat=True,
-            dropout=0.6,
-            add_self_loops=True,
-            bias=True,
-            return_attention_weights=True
-        )
-        self.conv3 = GATConv(
-            in_channels=64 * 8,
-            out_channels=64,
-            heads=8,
-            concat=False,
-            dropout=0.6,
-            add_self_loops=True,
-            bias=True,
-            return_attention_weights=True
-        )
+        self.node_encoder = Linear(encode_node_in, encode_node_out)
+        self.edge_encoder = Linear(encode_edge_in, encode_edge_out)
 
-        self.lin = Linear(64, 2)
-        self.linear = Linear(3, 512)
+        for i in range(len(input_sizes)):
+            if i < len(input_sizes) - 1:
+                self.conv.append(
+                    GATConv(
+                        in_channels=input_sizes[i],
+                        out_channels=out_channels,
+                        heads=heads,
+                        concat=concat_hidden_layer,
+                        add_self_loops=add_self_loops,
+                        bias=bias,
+                    )
+                )
+            else:
+                self.conv.append(
+                    GATConv(
+                        in_channels=input_sizes[i],
+                        out_channels=out_channels,
+                        heads=heads,
+                        concat=concat_out_layer,
+                        add_self_loops=add_self_loops,
+                        bias=bias,
+                    )
+                )
+
+        self.linear = Linear(16, 2)
+        self.skip_connection = Linear(encode_node_in, hidden_channels)
         self.edge_classifier = MLP(
-            in_channels=136,
-            hidden_channels=256,
-            out_channels=2,
-            num_layers=2,
-            bias=True
+            in_channels=out_channels + heads + out_channels,
+            hidden_channels=edge_hidden_channels,
+            out_channels=edge_out_channels,
+            num_layers=edge_num_layers,
+            bias=edge_bias,
         )
 
     def forward(self, x, edge_index, edge_attr, return_attention_weights):
-        out = self.node_encoder(x)
+        global alpha
+        node_embedding = self.node_encoder(x)
         edge_attr = self.edge_encoder(edge_attr)
+        # out = F.dropout(out, p=0.5, training=self.training)
 
-        out = F.dropout(out, p=0.3, training=self.training)
-        out, (_, alpha) = self.conv1(x=out,
-                                     edge_index=edge_index,
-                                     edge_attr=edge_attr,
-                                     return_attention_weights=True
-                                     )
-        out = F.elu(out)
-        out = F.dropout(out, p=0.3, training=self.training)
-        out = out + self.linear(x)
+        for i, gat_layer in enumerate(self.conv):
+            if i == len(self.conv) - 1:
+                node_embedding, (_, alpha) = gat_layer(
+                    x=node_embedding,
+                    edge_index=edge_index,
+                    edge_attr=edge_attr,
+                    return_attention_weights=return_attention_weights,
+                )
+            else:
+                node_embedding, (_, alpha) = gat_layer(
+                    x=node_embedding,
+                    edge_index=edge_index,
+                    edge_attr=edge_attr,
+                    return_attention_weights=return_attention_weights,
+                )
+                node_embedding = F.elu(node_embedding)
+                node_embedding = F.dropout(
+                    node_embedding, p=0.5, training=self.training
+                )
+                node_embedding = node_embedding + self.skip_connection(x)
 
-        out, (_, alpha) = self.conv2(x=out,
-                                     edge_index=edge_index,
-                                     edge_attr=edge_attr,
-                                     return_attention_weights=True
-                                     )
-        out = F.elu(out)
-        out = F.dropout(out, p=0.3, training=self.training)
-        out = out + self.linear(x)
-
-        out, (_, alpha) = self.conv3(x=out,
-                                     edge_index=edge_index,
-                                     edge_attr=edge_attr,
-                                     return_attention_weights=True
-                                     )
-        node_embedding = F.elu(out)
-        x_out = self.lin(node_embedding)
-
+        node_embedding = F.elu(node_embedding)
+        x_out = self.linear(node_embedding)
         node_embeds_src, node_embeds_dst = (
             node_embedding[edge_index[0]],
             node_embedding[edge_index[1]],
