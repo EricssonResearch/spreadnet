44f478f5a836df160dbf32f4469d6caf87ba4e0f
haodong_zhao <haodong.zhao.dev@outlook.com>
2022-10-27 19:49:28 +0200

    Create Class `WAndBModelTrainer`.
    Former-commit-id: ce45d45f943eb486b9146ceb4fc566a234191bfe

--- spreadnet/utils/model_trainer.py
+++ spreadnet/utils/model_trainer.py
@@ -9,6 +9,7 @@ from datetime import datetime
 from itertools import islice
 
 import torch
+import wandb
 from torch_geometric.loader import DataLoader
 from tqdm import tqdm
 import webdataset as wds
@@ -133,22 +134,7 @@ class ModelTrainer:
         )
         return train_loader, validation_loader
 
-    def execute(self, mode, epoch, total_epoch, dataloader, loss_func):
-        """
-        Execute training or validating.
-        Args:
-            epoch: current epoch
-            total_epoch: the number of total epoch
-            mode: train | validation
-            dataloader: dataloader
-            preprocessor: preprocess
-            model: model
-            loss_func: loss function
-            optimizer: optional optimizer for validation mode
-
-        Returns:
-            accuracy
-        """
+    def epoch_execute(self, mode, epoch, total_epoch, dataloader, loss_func):
         is_training = mode == "train"
         if is_training:
             pb_str = "Train"
@@ -200,13 +186,33 @@ class ModelTrainer:
 
         nodes_loss /= len(dataloader.dataset)
         edges_loss /= len(dataloader.dataset)
+        nodes_acc = (nodes_corrects / dataset_nodes_size).cpu().numpy()
+        edges_acc = (edges_corrects / dataset_edges_size).cpu().numpy()
+        return nodes_loss, edges_loss, nodes_acc, edges_acc
+
+    def execute(self, mode, epoch, total_epoch, dataloader, loss_func):
+        """
+        Execute training or validating.
+        Args:
+            epoch: current epoch
+            total_epoch: the number of total epoch
+            mode: train | validation
+            dataloader: dataloader
+            loss_func: loss function
+
+        Returns:
+            accuracy
+        """
+
+        is_training = mode == "train"
+        nodes_loss, edges_loss, nodes_acc, edges_acc = self.epoch_execute(
+            mode, epoch, total_epoch, dataloader, loss_func
+        )
 
         (self.losses_curve if is_training else self.validation_losses_curve).append(
             {"nodes": nodes_loss, "edges": edges_loss}
         )
 
-        nodes_acc = (nodes_corrects / dataset_nodes_size).cpu().numpy()
-        edges_acc = (edges_corrects / dataset_edges_size).cpu().numpy()
         (
             self.accuracies_curve if is_training else self.validation_accuracies_curve
         ).append({"nodes": nodes_acc, "edges": edges_acc})
@@ -247,7 +253,7 @@ class ModelTrainer:
         for epoch in range(epochs):
             self.steps_curve.append(epoch + 1)
 
-            self.execute("train", epoch, epochs, train_loader)
+            _ = self.execute("train", epoch, epochs, train_loader)
             validation_acc = self.execute(
                 "validation", epoch, epochs, validation_loader
             )
@@ -297,3 +303,109 @@ class ModelTrainer:
         weight_name = self.train_configs["best_weight_name"]
         torch.save(best_model_wts, os.path.join(self.model_save_path, weight_name))
         self.create_plot(plot_name)
+
+
+class WAndBModelTrainer(ModelTrainer):
+    def __init__(
+        self,
+        project_name: str,
+        experiment_name: str,
+        model_name: str,
+        model_configs: dict,
+        train_configs: dict,
+        dataset_path: str,
+        dataset_configs: dict,
+        model_save_path: str,
+    ):
+        super(WAndBModelTrainer, self).__init__(
+            model_name,
+            model_configs,
+            train_configs,
+            dataset_path,
+            dataset_configs,
+            model_save_path,
+        )
+        self.project_name = project_name
+        self.experiment_name = f"experiment_{model_name}"
+        self.wandb_configs = train_configs
+
+    def train(self):
+        wandb.login()
+        wandb.init(
+            # Set the project where this run will be logged
+            project=self.project_name,
+            name=f"{self.experiment_name}",
+            # Track hyperparameters and run metadata
+            config=self.wandb_configs,
+        )
+
+        print(f"Using {self.device} device...")
+
+        if not os.path.exists(self.model_save_path):
+            os.makedirs(self.model_save_path)
+
+        if not os.path.exists(self.plots_save_path):
+            os.makedirs(self.plots_save_path)
+
+        train_loader, validation_loader = self.construct_dataloader()
+
+        self.optimizer = torch.optim.Adam(
+            self.model.parameters(),
+            lr=self.train_configs["adam_lr"],
+            weight_decay=self.train_configs["adam_weight_decay"],
+        )
+
+        best_model_wts = copy.deepcopy(self.model.state_dict())
+        best_acc = 0.0
+
+        epochs = self.train_configs["epochs"]
+
+        for epoch in range(epochs):
+            (
+                train_nodes_loss,
+                train_edges_loss,
+                train_nodes_acc,
+                train_edges_acc,
+            ) = self.epoch_execute("train", epoch, epochs, train_loader)
+
+            train_metrics = {
+                "train/train_nodes_loss": train_nodes_loss,
+                "train/train_edges_loss": train_edges_loss,
+                "train/train_nodes_acc": train_nodes_acc,
+                "train/train_edges_acc": train_edges_acc,
+            }
+
+            (
+                validation_nodes_loss,
+                validation_edges_loss,
+                validation_nodes_acc,
+                validation_edges_acc,
+            ) = self.epoch_execute("validation", epoch, epochs, validation_loader)
+
+            validation_metrics = {
+                "validation/validation_nodes_loss": validation_nodes_loss,
+                "validation/validation_edges_loss": validation_edges_loss,
+                "validation/validation_nodes_acc": validation_nodes_acc,
+                "validation/validation_edges_acc": validation_edges_acc,
+            }
+
+            wandb.log({{**train_metrics, **validation_metrics}})
+
+            validation_acc = (validation_nodes_acc + validation_edges_acc) / 2
+
+            if validation_acc > best_acc:
+                best_acc = validation_acc
+                best_model_wts = copy.deepcopy(self.model.state_dict())
+
+            if epoch % self.train_configs["weight_save_freq"] == 0:
+                weight_name = f"model_weights_ep_{epoch}.pth"
+                torch.save(
+                    self.model.state_dict(),
+                    os.path.join(self.model_save_path, weight_name),
+                )
+
+        weight_name = self.train_configs["best_weight_name"]
+        torch.save(best_model_wts, os.path.join(self.model_save_path, weight_name))
+
+        # Mark the run as finished
+        wandb.finish()
