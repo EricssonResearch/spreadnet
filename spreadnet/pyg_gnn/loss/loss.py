110c8dfdc8942986f4b10511ad8837159804b8d1
Ishita Jaju <ishijaju@gmail.com>
2022-11-07 17:04:20 +0100

    Modify the loss computation
    Former-commit-id: 55a4e14154a8207cde17ff85783cf7c6cb451b20

--- spreadnet/pyg_gnn/loss/loss.py
+++ spreadnet/pyg_gnn/loss/loss.py
@@ -6,6 +6,42 @@
 import torch
 
 
+def get_masked_loss(
+    node_true, node_pred, node_losses, edge_true, edge_pred, edge_losses
+):
+    # getting the node and edge multiplication factor:
+    # ratio = nr_nodes_not_in_path / nr_nodes_in_path
+    count_nodes_in_path = torch.bincount(node_true)  # tensor([nr of 0's, nr of 1's])
+    node_mult_factor = count_nodes_in_path[0] / count_nodes_in_path[1]
+    count_edges_in_path = torch.bincount(edge_true)
+    edge_mult_factor = count_edges_in_path[0] / count_edges_in_path[1]
+
+    # converts logits to their classes
+    node_0 = node_pred[:, 0]
+    node_1 = node_pred[:, 1]
+    node_pred_classes = torch.where(node_0 > node_1, 0, 1)
+    # adds penalties to the losses with a node wrongly classified as "not in path"
+    updated_node_losses = torch.where(
+        torch.logical_and(node_pred_classes == 0, node_true == 1),
+        node_losses * node_mult_factor,
+        node_losses,
+    )
+    updated_node_losses = torch.mean(updated_node_losses)
+
+    # repeat for edges
+    edge_0 = edge_pred[:, 0]
+    edge_1 = edge_pred[:, 1]
+    edge_pred_classes = torch.where(edge_0 > edge_1, 0, 1)
+    updated_edge_losses = torch.where(
+        torch.logical_and(edge_pred_classes == 0, edge_true == 1),
+        edge_losses * edge_mult_factor,
+        edge_losses,
+    )
+    updated_edge_losses = torch.mean(updated_edge_losses)
+
+    return {"nodes": updated_node_losses, "edges": updated_edge_losses}
+
+
 def hybrid_loss(node_pred, edge_pred, node_true, edge_true):
     """A hybrid cross entropy loss combining edges and nodes.
 
@@ -22,16 +58,19 @@ def hybrid_loss(node_pred, edge_pred, node_true, edge_true):
 
     losses = {
         "nodes": torch.nn.functional.cross_entropy(
-            node_pred, node_true, reduction="mean"
+            node_pred, node_true, reduction="none"
         ),
         "edges": torch.nn.functional.cross_entropy(
-            edge_pred, edge_true, reduction="mean"
+            edge_pred, edge_true, reduction="none"
         ),
     }
+    penalized_losses = get_masked_loss(
+        node_true, node_pred, losses["nodes"], edge_true, edge_pred, losses["edges"]
+    )
 
     (_, corrects) = get_infers(node_pred, edge_pred, node_true, edge_true)
 
-    return losses, corrects
+    return penalized_losses, corrects
 
 
 def get_infers(node_pred, edge_pred, node_true, edge_true):
@@ -69,4 +108,5 @@ def get_infers(node_pred, edge_pred, node_true, edge_true):
             .to(node_pred.device)
         ),
     }
+
     return (infers, corrects)
