6be9239393ea3947027df87878dd0105fd99753d
ChanVuth Chea <cchvuth@gmail.com>
2022-11-10 08:09:06 +0100

    fix path
    Former-commit-id: 240de619cb61e34389d7f5902f0088d3d1aad6ce

--- sh/generate_dataset.sh
+++ sh/generate_dataset.sh
@@ -3,5 +3,5 @@
 #SBATCH -p core -n 1 -t 1-00:00:00
 #SBATCH -A uppmax2022-2-23 -M snowy
 
-cd ../
+cd ../experiments
 singularity exec --nv /proj/uppmax2022-2-23/spreadnet-runner-py3-pyg-cu116.sif python generate_dataset.py
