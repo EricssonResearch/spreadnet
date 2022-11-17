2c12c8287fef391ce7ceea8a81d5f39e0513aaba
Jennifer Gross <36174212+soothingjennyg@users.noreply.github.com>
2022-11-17 11:15:23 +0100

    now can run plot
    need to sort dataframe before plotting... it's a thing
    
    
    Former-commit-id: 08631d7fb597f629f4bc458ff354060f9a4715cd

--- experiments/runtime_experiments/runtime_analysis.py
+++ experiments/runtime_experiments/runtime_analysis.py
@@ -3,18 +3,36 @@ import matplotlib.pyplot as plt
 
 
 def main():
-    df = pd.read_csv("runtime_test_results.csv")
-    # df.plot()
-    # df['dijkstra_msp_memo'].plot(label='Dijkstra_MSP_memo', color='orange',
-    # index_col='min_nodes')
-    # df['dijkstra_memo'].plot(label='Dijkstra_Shortest_Path',
-    # color = 'purple', index_col='min_nodes')
-    plt.xlabel("Minimum Number of Nodes")
-    plt.ylabel("seconds")
+    data = pd.read_csv("runtime_test_results.csv")
+    df = pd.DataFrame(data)
+    df = df.sort_values(by=["min_nodes"])
+
+    print(df)
     df.plot(
-        x="min_nodes", y=["dijkstra_msp_memo", "dijkstra_memo", "Message Passing GNN"]
+        x="min_nodes",
+        y=["dijkstra_msp_memo", "dijkstra_memo", "Message Passing GNN"],
+        figsize=(10, 5),
+        grid=True,
     )
-    plt.legend()
+    # barplot = df.plot(x='min_nodes', columns=['dijkstra_msp_memo',
+    # 'dijkstra_memo', 'Message Passing GNN'])
+
+    # plt.show()
+    # index_col='min_nodes'
+    # df.plot()
+    # data = data.head()
+    # df = pd.DataFrame(data, columns=)
+    # plt.figure(figsize=(16, 8), dpi=150)
+    # dijkstra_msp_memo_runtime = df['dijkstra_msp_memo']
+    # .plot(label='Dijkstra_MSP_memo', color='orange')
+    # df['dijkstra_memo'].plot(label='Dijkstra_Shortest_Path',color = 'purple')
+    plt.title("Run Time for 20 Graphs")
+    plt.xlabel("Minimum Number of Nodes in Set")
+    plt.ylabel("Seconds")
+    # df.plot(
+    #    x="min_nodes", y=["dijkstra_msp_memo", "dijkstra_memo", "Message Passing GNN"]
+    # )
+    # plt.legend()
     plt.show()
     return
 
