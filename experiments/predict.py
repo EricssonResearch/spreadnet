8ae17842788a54e5627474ca0ea8b7c2f9e4bdd0
ChanVuth Chea <cchvuth@gmail.com>
2022-12-05 16:34:21 +0100

    unified predict.py
    Former-commit-id: 0b2da24ec5739e367baa54140b74228fde262725

--- experiments/predict.py
+++ experiments/predict.py
@@ -1,10 +1,9 @@
 """Use the trained model to do the prediction.
 
-Usage:
-    python predict.py [--config config_file_path] [--model weight_file_path]
-
-@Time    : 9/20/2022 8:28 PM
-@Author  : Haodong Zhao
+Args:
+    --model "MPNN|DeepGCN|GAT|DeepCoGCN"
+    --dataset-config dataset_config_path
+    --weight weight_file_name
 """
 import argparse
 from os import path as osp
@@ -18,7 +17,9 @@ import logging
 from joblib import Parallel, delayed
 from copy import deepcopy
 
-from spreadnet.pyg_gnn.models import EncodeProcessDecode
+from spreadnet.pyg_gnn.models import SPCoDeepGCNet, EncodeProcessDecode
+from spreadnet.pyg_gnn.models.deepGCN.sp_deepGCN import SPDeepGCN
+from spreadnet.pyg_gnn.models.graph_attention_network.sp_gat import SPGATNet
 from spreadnet.pyg_gnn.utils import get_correct_predictions
 from spreadnet.utils import yaml_parser
 from spreadnet.datasets.data_utils.processor import process_nx
@@ -36,10 +37,11 @@ from spreadnet.utils.post_processor import (
 torch.multiprocessing.set_start_method("spawn", force=True)
 
 default_yaml_path = osp.join(osp.dirname(__file__), "configs.yaml")
-default_dataset_yaml_path = osp.join(osp.dirname(__file__), "../dataset_configs.yaml")
+default_dataset_yaml_path = osp.join(osp.dirname(__file__), "dataset_configs.yaml")
 parser = argparse.ArgumentParser(description="Do predictions.")
+
 parser.add_argument(
-    "--config", default=default_yaml_path, help="Specify the path of the config file. "
+    "--model", help="Specify which model you want to train. ", required=True
 )
 parser.add_argument(
     "--dataset-config",
@@ -47,41 +49,111 @@ parser.add_argument(
     help="Specify the path of the dataset config file. ",
 )
 parser.add_argument(
-    "--model",
+    "--weight",
     default="model_weights_best.pth",
-    help="Specify the model we want to use.",
+    help="Specify the weight we want to use.",
 )
 args = parser.parse_args()
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-yaml_path = args.config
+
 dataset_yaml_path = args.dataset_config
 which_model = args.model
+which_weight = args.weight
+
+if which_model == "MPNN":
+    folder = "encode_process_decode"
+elif which_model == "DeepCoGCN":
+    folder = "co_graph_conv_network"
+elif which_model == "DeepGCN":
+    folder = "deep_graph_conv_network"
+elif which_model == "GAT":
+    folder = "graph_attention_network"
+else:
+    print("Please check the model name. -h for more details.")
+    exit()
+
+yaml_path = os.path.join(os.path.dirname(__file__), folder, "configs.yaml")
+weight_base_path = os.path.join(os.path.dirname(__file__), folder, "weights")
+predictions_path = osp.join(osp.dirname(__file__), folder, "predictions").replace(
+    "\\", "/"
+)
+log_save_path = osp.join(osp.dirname(__file__), folder, "logs").replace("\\", "/")
+
 configs = yaml_parser(yaml_path)
 dataset_configs = yaml_parser(dataset_yaml_path)
 train_configs = configs.train
 model_configs = configs.model
 data_configs = dataset_configs.data
-dataset_path = osp.join(
-    osp.dirname(__file__), "..", data_configs["dataset_path"]
-).replace("\\", "/")
-predictions_path = osp.join(osp.dirname(__file__), "predictions").replace("\\", "/")
-log_save_path = osp.join(osp.dirname(__file__), "logs").replace("\\", "/")
+dataset_path = osp.join(osp.dirname(__file__), data_configs["dataset_path"]).replace(
+    "\\", "/"
+)
 plot_size = 20
 
 if not os.path.exists(predictions_path):
     os.makedirs(predictions_path)
 
 
-def load_model(model_path):
+def load_model(model_name):
     """load the entire model.
 
-    :param model_path: the path of the model(entire model)
+    :param model_name: the name of the model
     :return: the loaded model
     """
-    model = torch.load(model_path)
-    return model
+    if model_name == "MPNN":
+        return EncodeProcessDecode(
+            node_in=model_configs["node_in"],
+            edge_in=model_configs["edge_in"],
+            node_out=model_configs["node_out"],
+            edge_out=model_configs["edge_out"],
+            latent_size=model_configs["latent_size"],
+            num_message_passing_steps=model_configs["num_message_passing_steps"],
+            num_mlp_hidden_layers=model_configs["num_mlp_hidden_layers"],
+            mlp_hidden_size=model_configs["mlp_hidden_size"],
+        ).to(device)
+    if model_name == "DeepCoGCN":
+        return SPCoDeepGCNet(
+            node_in=model_configs["node_in"],
+            edge_in=model_configs["edge_in"],
+            gcn_hidden_channels=model_configs["gcn_hidden_channels"],
+            gcn_num_layers=model_configs["gcn_num_layers"],
+            mlp_hidden_channels=model_configs["mlp_hidden_channels"],
+            mlp_hidden_layers=model_configs["mlp_hidden_layers"],
+            node_out=model_configs["node_out"],
+            edge_out=model_configs["edge_out"],
+        ).to(device)
+    if model_name == "GAT":
+        return SPGATNet(
+            num_hidden_layers=model_configs["num_hidden_layers"],
+            in_channels=model_configs["in_channels"],
+            hidden_channels=model_configs["hidden_channels"],
+            out_channels=model_configs["out_channels"],
+            heads=model_configs["heads"],
+            # dropout=model_configs[""],
+            add_self_loops=model_configs["add_self_loops"],
+            bias=model_configs["bias"],
+            edge_hidden_channels=model_configs["edge_hidden_channels"],
+            edge_out_channels=model_configs["edge_out_channels"],
+            edge_num_layers=model_configs["edge_num_layers"],
+            edge_bias=model_configs["edge_bias"],
+            encode_node_in=model_configs["encode_node_in"],
+            encode_edge_in=model_configs["encode_edge_in"],
+            encode_node_out=model_configs["encode_node_out"],
+            encode_edge_out=model_configs["encode_edge_out"],
+        ).to(device)
+    if model_name == "DeepGCN":
+        return SPDeepGCN(
+            node_in=model_configs["node_in"],
+            edge_in=model_configs["edge_in"],
+            encoder_hidden_channels=model_configs["encoder_hidden_channels"],
+            encoder_layers=model_configs["encoder_layers"],
+            gcn_hidden_channels=model_configs["gcn_hidden_channels"],
+            gcn_layers=model_configs["gcn_layers"],
+            decoder_hidden_channels=model_configs["decoder_hidden_channels"],
+            decoder_layers=model_configs["decoder_layers"],
+        ).to(device)
+    raise Exception("Invalid model name")
 
 
 def predict(model, graph):
@@ -97,7 +169,16 @@ def predict(model, graph):
     node_true, edge_true = graph.y
 
     # predict
-    (node_pred, edge_pred) = model(graph.x, graph.edge_index, graph.edge_attr)
+    if which_model == "GAT":
+        (node_pred, edge_pred) = model(
+            graph.x,
+            graph.edge_index,
+            graph.edge_attr,
+            return_attention_weights=model_configs["return_attention_weights"],
+        )
+    else:
+        (node_pred, edge_pred) = model(graph.x, graph.edge_index, graph.edge_attr)
+
     (infers, corrects) = get_correct_predictions(
         node_pred, edge_pred, node_true, edge_true
     )
@@ -123,23 +204,13 @@ if __name__ == "__main__":
     predict_logger.info(f"Using {device} device...")
     try:
         # load model
-        model = EncodeProcessDecode(
-            node_in=model_configs["node_in"],
-            edge_in=model_configs["edge_in"],
-            node_out=model_configs["node_out"],
-            edge_out=model_configs["edge_out"],
-            latent_size=model_configs["latent_size"],
-            num_message_passing_steps=model_configs["num_message_passing_steps"],
-            num_mlp_hidden_layers=model_configs["num_mlp_hidden_layers"],
-            mlp_hidden_size=model_configs["mlp_hidden_size"],
-        ).to(device)
+        print("Using model: ", which_model)
+        model = load_model(which_model)
 
-        weight_base_path = osp.join(
-            osp.dirname(__file__), train_configs["weight_base_path"]
+        weight_path = osp.join(weight_base_path, which_weight)
+        model.load_state_dict(
+            torch.load(weight_path, map_location=torch.device(device))
         )
-
-        model_path = osp.join(weight_base_path, which_model)
-        model.load_state_dict(torch.load(model_path, map_location=torch.device(device)))
         model.eval()
 
         raw_path = dataset_path + "/raw"
@@ -306,7 +377,7 @@ if __name__ == "__main__":
                     plt.clf()
                     print("Image saved at ", plot_name)
 
-                    # input("Press enter to predict another graph")
+                    input("Press enter to predict another graph")
     except Exception as e:
         predict_logger.exception(e)
 
