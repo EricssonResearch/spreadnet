9faa4fdd7d8e0be1afb945e662c9cec43f0c7252
ChanVuth Chea <cchvuth@gmail.com>
2023-01-04 11:30:51 +0100

    Co2
    Former-commit-id: cd75f4968e03f39134ef4948b65ae428f1d61212

--- spreadnet/datasets/data_utils/decoder.py
+++ spreadnet/datasets/data_utils/decoder.py
@@ -3,6 +3,7 @@ import torch
 
 
 def pt_decoder(key, data):
+    """Loads PyG data (.pt format) from TAR file."""
     if not key.endswith(".pt"):
         return None
     stream = io.BytesIO(data)
